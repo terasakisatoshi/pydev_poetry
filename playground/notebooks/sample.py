b55ab1c591486ac5978f26df96d7f016a334b50e
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-02-26 13:50:50 +0900

    add notebook

---
+++ playground/notebooks/sample.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.10.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %cd ../../
+# !poetry install
+# %cd -
+
+from hello import greet
+
+
