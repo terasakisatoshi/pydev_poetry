ad3104e6f5d1f8d806c3993a4d0e3f59e388286a
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-03-29 12:02:15 +0900

    remove setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-if __name__ == "__main__":
-    setup(
-    name="pydevpoetry"
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-)
